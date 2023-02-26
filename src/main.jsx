02536e13b7a9b409fda2d592f61eaf51b43281e8
Alex Toma <alex-toma0@users.noreply.github.com>
2023-02-26 17:55:00 +0200

    Removed React strictmode for debugging

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,5 @@ import App from './components/App'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 )
