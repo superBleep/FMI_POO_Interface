9ad27f705ed44adf5a843c3fe38e315c2a76cf33
nubaz <lucapaulf@gmail.com>
2023-02-28 21:52:48 +0200

    Fixed layout; Floating labels

--- src/components/EmailLogin.jsx
+++ src/components/EmailLogin.jsx
@@ -1,18 +1,17 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
 
 export default function EmailLogin() {
     return (
         <Form>
-            <Form.Group controlId='login-email' className='mb-3'>
-                <Form.Label>E-mail</Form.Label>
-                <Form.Control type='email' placeholder='Introdu adresa de e-mail...' />
-            </Form.Group>
+            <FloatingLabel label='E-mail' controlId='login-email' className='mb-3 text-black'>
+                <Form.Control type='email' placeholder='E-mail' />
+            </FloatingLabel>
             
-            <Form.Group controlId='login-pass' className='mb-3'>
-                <Form.Label>Parolă</Form.Label>
-                <Form.Control type='password' placeholder='Introdu parola...' />
-            </Form.Group>
+            <FloatingLabel label='Parolă' controlId='login-pass' className='mb-3 text-black'>
+                <Form.Control type='password' placeholder='Parolă' />
+            </FloatingLabel>
 
             <Form.Group controlId='login-remember' className='mb-3'>
                 <Form.Check type='checkbox' label='Ține-mă minte' />
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,16 +2,12 @@ import EmailLogin from "./EmailLogin";
 import GitHubLogin from "./GitHubLogin";
 
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 export default function Login() {
     return (
-        <Container className="position-absolute top-50 start-50 translate-middle text-white">
-            <Row>
-                <Col><EmailLogin /></Col>
-                <Col><GitHubLogin /></Col>
-            </Row>
+        <Container style={{width: '300px'}} className="position-absolute top-50 start-50 translate-middle text-white">
+                <EmailLogin />
+                <GitHubLogin />
         </Container>
     )
 }
