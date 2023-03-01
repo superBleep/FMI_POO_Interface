61d169c7f692985ae8aa3b319b74419d5f908ece
Alex Toma <alex-toma0@users.noreply.github.com>
2023-03-01 14:41:41 +0200

    Added functionality

--- src/components/GitHubLogin.jsx
+++ src/components/GitHubLogin.jsx
@@ -1,26 +1,45 @@
-import { useEffect } from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faGithub } from "@fortawesome/free-brands-svg-icons"
-const CLIENT_ID = "e74bc2c6cfd51906ab93" // client id for the GitHub oauth application
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;   
 export default function GitHubLogin() {
-    useEffect(() => {
-        // stores the authentication token for api calls in the codeParam variable (insecure, should be stored server-side)
-        const queryString = window.location.search
-        const urlParams = new URLSearchParams(queryString)
-        const codeParam = urlParams.get("code")
-        console.log(codeParam)
-    }, [])
-    let loginWithGithub = () => {
-        // login with GitHub prompt
-        window.location.assign(`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`)
+  const [accessToken, setToken] = useState("");
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const codeParam = urlParams.get("code");
+    console.log(codeParam);
+
+    if (codeParam && (localStorage.getItem("accesToken") === null)) {
+      async function getAccessToken() {
+        const tokenRoute = `http://localhost:4000/getAccessToken?code=${codeParam}`; 
+        await fetch(tokenRoute, { 
+          method: "GET"
+        }).then((response) => {
+          return response.json();
+        }).then((data) => {
+          console.log(data);
+          if (data.access_token) {
+            localStorage.setItem("accessToken", data.access_token);
+          }
+        });
+      }
+      getAccessToken();
     }
-    return (
-        <>
-        <button className='btn btn-primary' onClick={loginWithGithub}>
-            <FontAwesomeIcon icon={faGithub}/>
-            
-            Login with GitHub
-        </button>
-        </>
-    )
+  }, []);
+
+  let loginWithGithub = () => {
+    // login with GitHub prompt
+    window.location.assign(
+      `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`
+    );
+  };
+  return (
+    <>
+      <button className="btn btn-primary" onClick={loginWithGithub}>
+        <FontAwesomeIcon icon={faGithub} />
+        Login with GitHub
+      </button>
+    </>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
-)
+);
