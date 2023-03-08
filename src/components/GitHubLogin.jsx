c52b28d1d8ab782f4597f3865d495987f7927f23
Alex Toma <alex-toma0@users.noreply.github.com>
2023-03-08 12:09:27 +0200

    Added checks to ensure the user is in the database

--- src/components/GitHubLogin.jsx
+++ src/components/GitHubLogin.jsx
@@ -3,30 +3,55 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;   
 export default function GitHubLogin() {
-  const [accessToken, setToken] = useState("");
+  const [login, setLogin] = useState(false);
+  const [accessToken, setToken] = useState(null)
   useEffect(() => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const codeParam = urlParams.get("code");
     console.log(codeParam);
 
-    if (codeParam && (localStorage.getItem("accesToken") === null)) {
+    if (codeParam && (accessToken === null )) {
       async function getAccessToken() {
         const tokenRoute = `http://localhost:4000/getAccessToken?code=${codeParam}`; 
         await fetch(tokenRoute, { 
           method: "GET"
-        }).then((response) => {
-          return response.json();
-        }).then((data) => {
-          console.log(data);
+        })
+          .then((response) => response.json())
+          .then((data) => {
           if (data.access_token) {
-            localStorage.setItem("accessToken", data.access_token);
+            setToken(data.access_token);  
           }
         });
       }
       getAccessToken();
     }
-  }, []);
+    else if (accessToken) {
+      async function getUserData() {
+        const dataRoute = `http://localhost:4000/getUserData`;
+        await fetch(dataRoute, {
+          method: "GET",
+          headers: {
+            "Authorization": `Bearer ${accessToken}`
+          }
+        })
+        .then((response) => response.json()) 
+        .then((data) => {
+          // daca contul de github nu este in baza de date, loginul a esuat
+          const allowedUsers = ['test1','test2', 'alex-toma0']
+          if (!allowedUsers.includes(data.login)) {
+            window.alert("Contul nu este in baza de date! Loginul a esuat!");
+          }
+          else {
+            setLogin(true);
+          }
+        });
+      }
+      getUserData();
+      
+    } 
+  }, [accessToken,login]);
+
 
   let loginWithGithub = () => {
     // login with GitHub prompt
@@ -36,6 +61,8 @@ export default function GitHubLogin() {
   };
   return (
     <>
+      {login === true &&
+      <p>user is logged in!</p>}
       <button className="btn btn-primary" onClick={loginWithGithub}>
         <FontAwesomeIcon icon={faGithub} />
         Login with GitHub
