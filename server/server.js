4dda3d29ee4c9dbe86791b46f5e7fbbae41463e9
nubaz <lucapaulf@gmail.com>
2023-03-16 23:03:10 +0200

    Added Sequelize ORM

--- server/server.js
+++ server/server.js
@@ -1,11 +1,47 @@
 import express from 'express'
 import ViteExpress from 'vite-express'
 import path from 'path'
+import Sequelize, { DataTypes } from 'sequelize'
 
 const app = express()
 const port = 5173
+const __dirname = path.resolve(path.dirname(''))
+const sequilze = new Sequelize('postgres://fmi_app_db_admin:wLz*4p4M!dU9K$@localhost:5432/fmi_app_db')
 
-const __dirname = path.resolve(path.dirname(''));
+try {
+    await sequilze.authenticate()
+    console.log('Connected to database')
+} catch (e) {
+    console.error('Database: ', error)
+}
+
+const dbUser = sequilze.define('dbUser', {
+    surname: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+    },
+    initial: {
+        type: DataTypes.STRING(4)
+    },
+    email: {
+        type: DataTypes.STRING(70),
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    }
+}, {
+    tableName: 'dbuser',
+    timestamps: false
+})
+
+const users = await dbUser.findAll()
+console.log(JSON.stringify(users, null, 2))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
