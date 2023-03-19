2d631eea68ed541dd73dd03d6522b8c39501186d
Alex Toma <alex-toma0@users.noreply.github.com>
2023-03-19 17:36:33 +0200

    Stored token in localStorage

--- src/components/GitHubLogin.jsx
+++ src/components/GitHubLogin.jsx
@@ -4,14 +4,12 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;   
 export default function GitHubLogin() {
   const [login, setLogin] = useState(false);
-  const [accessToken, setToken] = useState(null)
+  const [userData, setUserData] = useState(null);
   useEffect(() => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const codeParam = urlParams.get("code");
-    console.log(codeParam);
-
-    if (codeParam && (accessToken === null )) {
+    if (codeParam && (localStorage.getItem("accessToken") === null )) {
       async function getAccessToken() {
         const tokenRoute = `http://localhost:4000/getAccessToken?code=${codeParam}`; 
         await fetch(tokenRoute, { 
@@ -20,19 +18,19 @@ export default function GitHubLogin() {
           .then((response) => response.json())
           .then((data) => {
           if (data.access_token) {
-            setToken(data.access_token);  
+            localStorage.setItem("accessToken", data.access_token);
           }
         });
       }
       getAccessToken();
     }
-    else if (accessToken) {
+    else if (localStorage.getItem("accessToken")) {
       async function getUserData() {
         const dataRoute = `http://localhost:4000/getUserData`;
         await fetch(dataRoute, {
           method: "GET",
           headers: {
-            "Authorization": `Bearer ${accessToken}`
+            "Authorization": `Bearer ${localStorage.getItem("accessToken")}`
           }
         })
         .then((response) => response.json()) 
@@ -44,13 +42,14 @@ export default function GitHubLogin() {
           }
           else {
             setLogin(true);
+            setUserData(data);
           }
         });
       }
       getUserData();
       
     } 
-  }, [accessToken,login]);
+  }, [userData,login]);
 
 
   let loginWithGithub = () => {
@@ -62,7 +61,7 @@ export default function GitHubLogin() {
   return (
     <>
       {login === true &&
-      <p>user is logged in!</p>}
+      <p> {userData["login"]} is logged in!</p>}
       <button className="btn btn-primary" onClick={loginWithGithub}>
         <FontAwesomeIcon icon={faGithub} />
         Login with GitHub
